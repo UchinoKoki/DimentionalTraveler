5d6d0e079fd441e727183031a0f6c75bd6f69961
UchinoKoki <hw21a023@oecu.jp>
2024-02-21 05:08:25 +0900

    ゲート起動時のエネミーポップ部分だけ制作

---
+++ Assets/Scripts/Common/Field/Gate/GatePopEnemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatePopEnemy : MonoBehaviour
+{
+    [SerializeField] private EnemyListAsset enemyListAsset;
+
+    private float popTimer;
+    [SerializeField] private float popTime;
+    [SerializeField] private float popQuantity;
+    [SerializeField] bool isOn = false;
+
+    [SerializeField] private float popAreaRadius = 1f;
+
+    private void FixedUpdate() {
+        if (!isOn) return;
+        if (popTimer > 0) {
+            popTimer -= Time.deltaTime;
+        } else {
+            for (int i = 0; i < popQuantity; i++) {
+                int randomIndex = Random.Range(0, enemyListAsset.EnemyList.Count);
+                Vector3 _pos = new Vector3(transform.position.x + Random.Range(-popAreaRadius, popAreaRadius), transform.position.y, transform.position.z + Random.Range(-popAreaRadius, popAreaRadius));
+                GameObject enemy = Instantiate(enemyListAsset.EnemyList[randomIndex].enemyObject, _pos, Quaternion.identity);
+            }
+            popTimer = popTime;
+        }
+    }
+    public void TurnOnGate()
+    {
+        isOn = true;
+    }
+}
