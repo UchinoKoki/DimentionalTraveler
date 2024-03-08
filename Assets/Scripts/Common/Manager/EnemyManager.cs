c0de40a5653aa566fa49364eea977cbd771b25a3
UchinoKoki <hw21a023@oecu.jp>
2024-03-09 01:43:34 +0900

    EnemyListを最適化

--- Assets/Scripts/Common/Manager/EnemyManager.cs
+++ Assets/Scripts/Common/Manager/EnemyManager.cs
@@ -21,6 +21,23 @@ public class EnemyManager : MonoBehaviour
     }
     public void AddEnemy(BaseCharacter _enemy)
     {
-        enemyList.Add(_enemy);
+        var _setList = enemyList;
+        _setList.Add(_enemy);
+        enemyList = DeleteNullToList(_setList);
+    }
+    public void DelEnemy(BaseCharacter _enemy)
+    {
+        var _setList = enemyList;
+        _setList.Remove(_enemy);
+        enemyList = DeleteNullToList(_setList);
+    }
+    public List<BaseCharacter> GetEnemyList()
+    {
+        return DeleteNullToList(enemyList);
+    }
+    private List<BaseCharacter> DeleteNullToList(List<BaseCharacter> _enemyList)
+    {
+        _enemyList.RemoveAll(enemy => enemy == null);
+        return _enemyList;
     }
 }
