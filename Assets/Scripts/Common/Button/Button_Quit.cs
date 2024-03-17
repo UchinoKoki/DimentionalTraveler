fb6d62b9979a8f8ec4e12f3525d28596c3af8417
UchinoKoki <hw21a023@oecu.jp>
2024-03-18 02:40:03 +0900

    終了ボタンの実装

---
+++ Assets/Scripts/Common/Button/Button_Quit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Quit : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Application.Quit();
+    }
+}
